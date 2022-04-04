0112aff4ee6ea75854c9746af8b882bbb801fdb0
Liam Chambers <chambersl@pjas.org>
2022-04-04 00:51:12 -0400

    Initial customizations and setup

--- App.js
+++ App.js
@@ -1,36 +1,193 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
+import {
+  Alert,
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 
+// FILE IMPORTS
+const config = require('./config')
+const logo = require("./assets/images/logo.png")
+
+
+// HOME SCREEN
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
-    </View>
+    <View style = {styles.container}>
+        
+      {/* Logo */}
+        <Image source = {logo} style = {styles.mainPageLogo}  fadeDuration = {config.fadeDuration} /> 
+      {/* Welcome Message */}
+        <Text style = {styles.homeTitle}>
+          Welcome to the {config.stateMeetYear} PJAS State Meeting app.
+        </Text>
+
+
+      {/* Stylized Text */}
+        <Text style = {styles.h1}>
+          H1 Text
+        </Text>
+        <Text style = {styles.h2}>
+          H2 Text
+        </Text>
+        <Text style = {styles.h3}>
+          H3 Text
+        </Text>
+        <Text style = {styles.defaultText}>
+          Normal size text
+        </Text>
+
+        {/* ABOUT PAGE BUTTON */}
+        <TouchableOpacity
+          onPress={() => navigation.navigate('About')}
+          style ={styles.button}>
+            <Text style = {styles.buttonText}>
+              About This App
+            </Text>
+        </TouchableOpacity>
+
+
+        <TouchableOpacity
+          onPress = {() =>
+            Alert.alert(`Popup!`, `This is a test alert!`, [
+              { text: "Yes" },
+              { text: "No" },
+            ])
+          }
+          style ={styles.button}>
+            <Text style = {styles.buttonText}>
+              Test: alert popup with buttons inside
+            </Text>
+        </TouchableOpacity>
+
+
+        {/* Status Bar */}
+        <StatusBar style = "auto" />
+      </View>
   );
 }
 
+
+// SECOND SCREEN
 function NotificationsScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress = {() => navigation.goBack()} title = "Go back home" />
     </View>
   );
 }
 
+
+
+
+// DRAWER
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      <Drawer.Navigator
+        screenOptions = {{
+          overlayColor: config.pjasBlue75percent,
+          itemStyle: {marginVertical: 5},
+        }}
+        initialRouteName = "Home"
+      >
+        <Drawer.Screen name = "Home" component = {HomeScreen} />
+        <Drawer.Screen name = "Notifications" component = {NotificationsScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+
+
+
+
+
+
+
+
+/****************************************/
+/* STYLES                               */
+/****************************************/
+const styles = StyleSheet.create({
+  /****************************************/
+  /* CONTAINERS                           */
+  /****************************************/
+  container: {
+    flex: 1,
+    backgroundColor: config.defaultBkgColor,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    padding: 20,
+  },
+  /****************************************/
+  /* BUTTONS                              */
+  /****************************************/
+  button: {
+    backgroundColor: config.pjasBlue,
+    padding: config.buttonPadding,
+    borderRadius: config.buttonBorderRadius,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  buttonText: {
+    fontSize: config.normTextSize,
+    color: "white",
+  },
+  /****************************************/
+  /* IMAGES                               */
+  /****************************************/
+  mainPageLogo: {
+    width: 150,
+    height: 150,
+    marginBottom: 50,
+  },
+  /****************************************/
+  /* TEXT / FONTS                         */
+  /****************************************/
+  defaultText: {
+    color: config.pjasBlue,
+    fontSize: config.normTextSize,
+    marginHorizontal: 15
+  },
+  homeTitle: {
+    textAlignVertical: "center",
+    textAlign: "center",
+    color: config.pjasBlue,
+    fontSize: config.h1TextSize,
+    fontWeight: 'bold',
+    marginHorizontal: 15,
+    marginBottom: 50,
+  },
+
+    /****************************************/
+    /* HEADINGS                             */
+    /****************************************/
+    h1: {
+      color: config.pjasBlue,
+      fontSize: config.h1TextSize,
+      fontWeight: 'bold',
+      marginHorizontal: 15
+    },
+    h2: {
+      color: config.pjasBlue,
+      fontSize: config.h2TextSize,
+      fontWeight: 'bold',
+      marginHorizontal: 15
+    },
+    h3: {
+      color: config.pjasBlue,
+      fontSize: config.h3TextSize,
+      fontWeight: 'bold',
+      marginHorizontal: 15
+    },
+
+});
---
+++ config.js
@@ -0,0 +1,25 @@
+const config = {
+  /* TEXT */
+  stateMeetYear: '2022',
+
+  /* COLORS */
+  pjasBlue: '#122937',
+  pjasBlue75percent: '#122937BF',
+  defaultBkgColor: '#eee',
+
+
+  /* FONT SIZES */
+  normTextSize: 16,
+  h1TextSize: 28,
+  h2TextSize: 25,
+  h3TextSize: 22,
+
+  /* BUTTONS */
+  buttonPadding: 20,
+  buttonBorderRadius: 5,
+ 
+  /* EFFECTS */
+  fadeDuration: 500,
+};
+
+module.exports = config;
